caf3be4eecde5906169278f7017f154f39d14fb3
Forszt Mihály <mihalyforszt@outlook.hu>
2022-03-29 13:46:35 +0200

    Tartalom hozzáadása a kezdőoldalhoz, FAQ page hozzáadása

--- OnlineCoursesWeb/Controllers/HomeController.cs
+++ OnlineCoursesWeb/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace OnlineCoursesWeb.Controllers
         {
             return View();
         }
+        
+        public IActionResult FAQ()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
